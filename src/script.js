ef7c62ac3cdecbf704871ac175a12d153e77f2b5
Adega318 <eadegafernandez@gmail.com>
2024-02-17 21:36:58 +0100

    licences+

--- src/script.js
+++ src/script.js
@@ -3,7 +3,7 @@
 // SPDX-FileCopyrightText: 2024 Enrique Adega
 // SPDX-FileCopyrightText: 2024 Guillen Porto
 //
-// SPDX-License-Identifier: GPL-3.0
+// SPDX-License-Identifier: Apache-2.0
 
 function init(){
 	localStorage.setItem("Nevera", "0");
